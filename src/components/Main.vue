3f287f575b3366b7adbe43093412aa1b62e18f92
henrikts99 <henriko99@live.no>
2026-03-02 16:28:12 +0100

    Text interpolation

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 const name = ref('Henrik');
+const quote = ref("First, solve the problem. Then write the code.");
+
+name.value = "Ikke Henrik";
 </script>
 
 <template>
     <main>
         <h1>Hello, {{ name }}!</h1>
+        <p>{{ quote }}</p>
     </main>
 
 </template>
