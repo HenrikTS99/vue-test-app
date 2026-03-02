6ea7b41fae92c34072f1e9bb28f5113f45a9de8e
henrikts99 <henriko99@live.no>
2026-03-02 16:30:10 +0100

    Attribute binding

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 const copyright_year = ref('2025');
+const href = ref("https://scrimba.com")
 </script>
 
 <template>
     <footer>
         <p>&copy; {{ copyright_year }}</p>
+        <p>Created by <a v-bind:href="href"><span>Professor Jeffè</span></a></p>
     </footer>
 
 </template>
 
-<style scoped></style>
+<style scoped>
+a {
+    color: white;
+}
+</style>
