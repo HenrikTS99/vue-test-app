298368e29718394887f127cbe7413b39a7ace862
henrikts99 <henriko99@live.no>
2026-03-02 15:44:46 +0100

    add Header, Main and Footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const copyright_year = ref('2025');
+</script>
+
+<template>
+    <footer>
+        <p>&copy; {{ copyright_year }}</p>
+    </footer>
+
+</template>
+
+<style scoped></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const name = ref('Henrik');
+</script>
+
+<template>
+    <main>
+        <h1>Hello, {{ name }}!</h1>
+    </main>
+
+</template>
+
+<style scoped></style>
