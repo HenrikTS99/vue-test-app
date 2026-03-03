6f73d7aded7146b425794e66b634bc911f030cb0
henrikts99 <henriko99@live.no>
2026-03-03 11:02:33 +0100

    Passing props trough components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 
 import Header from '@/components/Header.vue';
+import Props from '@/components/Props.vue';
 import Main from '@/components/Main.vue';
 import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
     <Header />
+    <Props title="Test Props" subtitle="Test subtitle" />
     <Main />
     <Footer />
 </template>
---
+++ src/components/Props.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+defineProps({
+    title: {
+        type: String,
+        default: 'Default message'
+    },
+
+    subtitle: {
+        type: String,
+        default: 'Default message subtitle'
+    }
+});
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+    <h3>{{ subtitle }}</h3>
+</template>
