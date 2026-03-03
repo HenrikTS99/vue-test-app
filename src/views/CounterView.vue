94e9d6685ec42e852a33990cb5e2e1f0a0de1d94
henrikts99 <henriko99@live.no>
2026-03-03 13:29:09 +0100

    Counter view. Increment and decrement, including computed and watch properties

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,8 +1,23 @@
-<script lang="ts">
+<script setup lang="ts">
+import { ref, computed, watch } from 'vue';
+const count = ref<number>(0)
 
+const doubledCount = computed(() => count.value * 2);
+
+watch(count, (newValue, oldValue) => console.log('Count updated to: ' + newValue + ". Old value was " + oldValue));
+
+const increment = () => {
+    count.value++;
+}
+const decrement = () => {
+    count.value--;
+}
 </script>
 
 
 <template>
-
+    <h2>Count: {{ count }}</h2>
+    <h3>Count doubled: {{ doubledCount }}</h3>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
 </template>
