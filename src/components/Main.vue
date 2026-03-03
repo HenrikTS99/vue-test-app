0978fa1eac25fb6114c224b26a5846148f8675bd
henrikts99 <henriko99@live.no>
2026-03-03 09:59:02 +0100

    if, else and for loops

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,6 +4,8 @@ import image_1 from '@/assets/images/a_close_up_of_a_plane.jpg';
 const name = ref('Henrik');
 const quote = ref("First, solve the problem. Then write the code.");
 const isBtnDisabled = ref(true)
+const status = ref(false)
+const tasks = ['Task one', 'Task two', 'Task Three']
 
 name.value = "Ikke Henrik";
 </script>
@@ -12,16 +14,24 @@ name.value = "Ikke Henrik";
     <main>
         <h1>Hello, {{ name }}!</h1>
         <p>{{ quote }}</p>
+
+        <p v-if="status">Status true!</p>
+        <p v-else>Status false!</p>
+
+        <h2>Tasks:</h2>
+        <ul>
+            <li v-for="task in tasks" :key="task">{{ task }}</li>
+        </ul>
         <section id="buttons">
             <!-- Shorthand v-bind:  :disabled-->
             <button v-bind:disabled="isBtnDisabled">Another!</button>
         </section>
-        <section>
-            <img src="@/assets/images/a_close_up_of_a_plane.jpg" alt="" />
-        </section>
-        <section>
-            <img v-bind:src="image_1" alt="" />
-        </section>
+        <!-- <section> -->
+        <!--     <img src="@/assets/images/a_close_up_of_a_plane.jpg" alt="" /> -->
+        <!-- </section> -->
+        <!-- <section> -->
+        <!--     <img v-bind:src="image_1" alt="" /> -->
+        <!-- </section> -->
     </main>
 
 </template>
